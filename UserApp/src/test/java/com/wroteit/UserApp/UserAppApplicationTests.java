05cb772b4a6d0da536e78f9dbc8761ecfa44d01f
Shark23p4 <yelsharkawy42@gmail.com>
2025-05-06 15:59:02 +0300

    Initialize UserApp

---
+++ UserApp/src/test/java/com/wroteit/UserApp/UserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wroteit.UserApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
