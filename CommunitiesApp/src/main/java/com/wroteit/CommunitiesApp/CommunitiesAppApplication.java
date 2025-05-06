12ab23b3bcc036848deb57f13bfbefbbb9723e69
Shark23p4 <yelsharkawy42@gmail.com>
2025-05-06 16:00:06 +0300

    Initialize Communities App

---
+++ CommunitiesApp/src/main/java/com/wroteit/CommunitiesApp/CommunitiesAppApplication.java
@@ -0,0 +1,13 @@
+package com.wroteit.CommunitiesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitiesAppApplication.class, args);
+	}
+
+}
---
+++ CommunitiesApp/src/test/java/com/wroteit/CommunitiesApp/CommunitiesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wroteit.CommunitiesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitiesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
