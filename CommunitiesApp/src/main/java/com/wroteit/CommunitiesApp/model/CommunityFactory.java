430df6673219cd124ccdba1189b027107056b2da
Shark23p4 <yelsharkawy42@gmail.com>
2025-05-20 17:12:54 +0300

    Set up parent pom.xml and ensured project build

--- CommunitiesApp/src/main/java/com/wroteit/CommunitiesApp/model/CommunityFactory.java
+++ CommunitiesApp/src/main/java/com/wroteit/CommunitiesApp/model/CommunityFactory.java
@@ -10,10 +10,10 @@ public class CommunityFactory {
     public Community create(String name, String description, CommunityType type) {
         Community community = new Community(name, description);
         switch (type) {
-            case FOOD -> community.setTags(new ArrayList<>(List.of("food", "nutrition")));
-            case SPORTS -> community.setTags(new ArrayList<>(List.of("sports", "fitness")));
-            case GAMING -> community.setTags(new ArrayList<>(List.of("gaming", "fun")));
-            case POLITICS -> community.setTags(new ArrayList<>(List.of("politics", "important")));
+            case FOOD: community.setTags(new ArrayList<>(List.of("food", "nutrition")));
+            case SPORTS: community.setTags(new ArrayList<>(List.of("sports", "fitness")));
+            case GAMING: community.setTags(new ArrayList<>(List.of("gaming", "fun")));
+            case POLITICS: community.setTags(new ArrayList<>(List.of("politics", "important")));
         }
         return community;
     }
